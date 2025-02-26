336160e31e330b557189cd280dc2ed72c5ec9682
Annelie Svensson <annelievs@hotmail.com>
2025-02-26 15:09:10 +0100

    Add project files.

---
+++ Data/MusicContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moment_4_MusicApi.Models;
+
+namespace Moment_4_MusicApi.Data;
+
+public class MusicContext : DbContext
+{
+    public MusicContext(DbContextOptions<MusicContext> options) : base(options) { }
+
+    public DbSet<Music> Music { get; set; }
+}
---
+++ Models/Music.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moment_4_MusicApi.Models;
+
+public class Music
+{
+    //Properties
+    public int MusicId { get; set; }
+
+    [Required]
+    public string? Artist { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    [Required]
+    public int? Length { get; set; }
+    [Required]
+    public string? Category { get; set; }
+
+}
